665c959ddde3c79a1e0dfd7bf509aa1adf678bfb
teacher Marcos <teacher.mrdv@gmail.com>
2023-10-20 12:09:41 +0800

    Create Chapter4Problem3.java

---
+++ worksheets/answers/Chapter4Problem3.java
@@ -0,0 +1,18 @@
+
+public class Chapter4Problem3 {
+	
+	public static void main (String[] args) {
+		/*
+		 * input 1 numbers: N
+		 * < 1000
+		 *
+		 * search for x and y so that
+		 * x^2 + y^2 = N
+		 *
+		 * output x and y if x^2 + y^2 = N
+		 * or output "impossible" otherwise
+		 *
+		 */
+	}
+}
+
