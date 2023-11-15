ba81f34a9678ed59e66db69e6aa7dbd3114484ab
teacher Marcos <teacher.mrdv@gmail.com>
2023-11-15 11:46:21 +0800

    Create InputDemo.java

---
+++ demos/InputDemo.java
@@ -0,0 +1,15 @@
+
+
+public class InputDemo {
+	
+	public static void main (String[] args) {
+		int n = IBIO.inputInt("Input an integer: ");
+		double x = IBIO.inputDouble("Input an decimal: ");
+		IBIO.out("IBIO.out() = System.out.print()  ");
+		IBIO.output("IBIO.output() = System.out.println()");
+		IBIO.out("n = ");
+		IBIO.output(n);
+		IBIO.output("x = " + x);
+	}
+}
+
