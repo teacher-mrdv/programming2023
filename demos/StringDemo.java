e52d431c3598a6a854388b580fb332ed94bea450
teacher Marcos <teacher.mrdv@gmail.com>
2024-01-09 19:48:43 +0800

    Update StringDemo.java
    addded output

--- demos/StringDemo.java
+++ demos/StringDemo.java
@@ -5,18 +5,21 @@ public class StringDemo {
 		String name = " Marcos";
 		String surname = " Drien ";
 		String fullName = name + surname;
-		System.out.println("Mr " + name); // prepend
-		System.out.println(name + surname ); // apppend
-		System.out.println( fullName.trim() ); // prepend
+		System.out.println("String name = \" Marcos\";\nString surname = \" Drien \";\nString fullName = name + surname;\n");
+		System.out.println(">>> String concatenation\n");
+		System.out.println("\"Mr\" + name: " + "Mr" + name); // prepend
+		System.out.println("name + surname: " + name + surname ); // apppend
+		System.out.println( "fullName.trim(): " + fullName.trim() ); // trim (removes spaces at the beginning and end of a String)
 		String me = "Marcos";
 		String he = me;
 		String you = new String("Marcos");
-		System.out.println(name == me);
-		System.out.println(me == he);
-		System.out.println(you == me);
-		System.out.println(name.equals(me) );
-		System.out.println(me.equals(he) );
-		System.out.println(you.equals(me) );
+		System.out.println("\n>>> String equalities\n");
+		System.out.println("name == me: " + (name == me));
+		System.out.println("me == he: " + (me == he));
+		System.out.println("you == me: " + (you == me));
+		System.out.println( "name.equals(me): " + name.equals(me) );
+		System.out.println( "me.equals(he): " + me.equals(he) );
+		System.out.println( "you.equals(me): " + you.equals(me) );
 	}
 }
 
