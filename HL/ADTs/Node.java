c64b4c06d5580d69778cfe54393fac3224a8ce24
teacher Marcos <teacher.mrdv@gmail.com>
2024-01-10 12:11:23 +0800

    Create Node.java
    HL only - ADTs unit

---
+++ HL/ADTs/Node.java
@@ -0,0 +1,32 @@
+/*
+ * This is the definition (class) of a node for an ADT
+ * Singly-linked node
+ * 
+ */
+
+public class Node
+{
+	int data;
+	Node next; // self-reference (Node refers/links/points to another node)
+
+	// empty constructor (OOP) creates an empty node
+	public Node() { }
+
+	// custom constructor that creates and initialises a node
+	public Node(int d)
+	{
+		data = d;
+	}
+
+	// is the (current) node linked to another node or does it point to null?
+	public boolean hasNext()
+	{	return next != null;
+	}
+
+	// this method converts a Node object to a String representation
+	public String toString()
+	{
+		return this.data + "";
+	}
+}
+
