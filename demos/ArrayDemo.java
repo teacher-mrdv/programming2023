9cdf6d4df92ed572ea49ba4fb1e4b8f9e81aed20
teacher Marcos <teacher.mrdv@gmail.com>
2024-01-23 12:01:33 +0800

    Create ArrayDemo.java

---
+++ demos/ArrayDemo.java
@@ -0,0 +1,15 @@
+
+public class ArrayDemo {
+	
+	public static void main (String[] args) {
+		int x = -1;
+		String s; //null/not initialised
+		s=""; // empty string
+		int[] numbers = new int[10];
+		System.out.println(x);
+		System.out.println(s);
+		System.out.println(numbers);
+		
+	}
+}
+
