d60deb2f11d099f9a07eaa2f4d173734ab8e25b1
teacher Marcos <teacher.mrdv@gmail.com>
2024-01-24 08:55:21 +0800

    Update ArrayDemo.java

--- demos/ArrayDemo.java
+++ demos/ArrayDemo.java
@@ -1,3 +1,4 @@
+//import java.util.Arrays;
 
 public class ArrayDemo {
 	
@@ -5,11 +6,26 @@ public class ArrayDemo {
 		int x = -1;
 		String s; //null/not initialised
 		s=""; // empty string
-		int[] numbers = new int[10];
+		//int[] numbers = new int[10];
+		int[] numbers = { 1,2,3,4,5,6,7,8,9,10 };
 		System.out.println(x);
 		System.out.println(s);
 		System.out.println(numbers);
-		
+		//System.out.println(Arrays.toString(numbers)); // you can't touch this
+		for(int index = 0; index < numbers.length; index++) {
+			numbers[index] = numbers[index] * 3;
+		}
+		for(int index = 0; index < numbers.length; index++) {
+			System.out.println("[" + index + "] " + numbers[index] + " ");
+		}
+		System.out.println("\n\nIn reverse:");
+		for(int index = numbers.length-1; index >= 0 ; index--) {
+			System.out.println("[" + index + "] " + numbers[index] + " ");
+		}
+		System.out.println("\n\n");
+		for(int index = 0; index < numbers.length; index++) {
+			System.out.println(numbers[index] + ' ');
+		}
 	}
 }
 
