9e9aa7be31a0467b17828deb6c91c719a966be57
teacher Marcos <teacher.mrdv@gmail.com>
2024-03-06 18:17:47 +0800

    Update Queue.java

--- HL/ADTs/Queue.java
+++ HL/ADTs/Queue.java
@@ -18,11 +18,27 @@ public class Queue
 
 	public void enQueue(int data) {
 		// your code goes here
+		Node newNode = new Node(data);
+		if( isEmpty() ) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			tail.next = newNode;
+			tail = newNode;
+		}
+		size++;
 	}
 
 	public int deQueue() {
-		// your code goes here
-		return 0; // <-- change the 0...
+		if( isEmpty() == false ) {
+			int data = head.data;
+			head = head.next;
+			size--;
+			return data;
+		} else {
+			System.out.println("The queue is empty");
+			return 0;
+		}
 	}
 
 	public void printQueue() {
