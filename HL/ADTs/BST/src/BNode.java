1c03d36df73141116ce9b004eb1b970370ef0a0e
teacher Marcos <teacher.mrdv@gmail.com>
2024-08-30 09:49:07 +0800

    BST WIP

--- HL/ADTs/BST/src/BNode.java
+++ HL/ADTs/BST/src/BNode.java
@@ -3,10 +3,15 @@ public class BNode {
     BNode left;
     BNode right;
 
+    // constructor
     public BNode(int data) {
         this.data = data;
         this.left = null;
         this.right = null;
+    }
+
+    public String toString() {
+        return Integer.toString(data); // also data + "";
     }
 
 }
--- HL/ADTs/BST/src/BSTree.java
+++ HL/ADTs/BST/src/BSTree.java
@@ -1,3 +1,37 @@
 public class BSTree {
+    //field/attribute/instance variable
     BNode root;
+
+    //constructors (overloaded)
+    public BSTree(int data) {
+        BNode newNode = new BNode(data);
+        root = newNode;
+    }
+
+    public BSTree(BNode newNode) {
+        root = newNode;
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+    public void addNode(int data) {
+        BNode newBNode = new BNode(data);
+        addNode(root, newBNode);
+    }
+
+    public void addNode(BNode newBNode) {
+        addNode(root, newBNode);
+    }
+
+    public void addNode(BNode node, BNode newNode) {
+        if( node == null ) {
+            node = newNode;
+            return;
+        } else if(newNode.data < node.data) {
+            addNode(node.left, newNode);
+        } else if(newNode.data > node.data) {
+            addNode(node.right, newNode);
+        }
+    }
 }
