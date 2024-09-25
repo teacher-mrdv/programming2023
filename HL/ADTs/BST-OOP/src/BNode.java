897fc3dc5f0b87e0a9f8e60c8b03e2191bdbc046
teacher Marcos <teacher.mrdv@gmail.com>
2024-09-25 13:19:43 +0800

    BST encapsulated

---
+++ HL/ADTs/BST-OOP/src/BNode.java
@@ -0,0 +1,41 @@
+public class BNode {
+    private int data;
+    private BNode left;
+    private BNode right;
+
+    // constructor
+    public BNode(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public BNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(BNode left) {
+        this.left = left;
+    }
+
+    public BNode getRight() {
+        return right;
+    }
+
+    public void setRight(BNode right) {
+        this.right = right;
+    }
+
+    public String toString() {
+        return Integer.toString(data); // also data + "";
+    }
+
+}
