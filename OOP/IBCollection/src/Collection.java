adfa0f79eeaa5a9de3ef13c2e30803ab7da8c3c9
mrDV <48473242+teacher-mrdv@users.noreply.github.com>
2025-02-26 16:10:32 +0800

    Collection ADT Java

--- OOP/IBCollection/src/Collection.java
+++ OOP/IBCollection/src/Collection.java
@@ -1,4 +1,8 @@
-
+/************************************************************
+ *	Collection Java class									*
+ *	to simulate/implement the IBO collection data structure	*
+ *  02.2025													*
+ ************************************************************/
 public class Collection
 {
 	private Element start;
@@ -12,7 +16,6 @@ public class Collection
 	public boolean isEmpty()
 	{	return start == null;
 	}
-
 
 	public Element getStart() {
 		return start;
@@ -37,7 +40,7 @@ public class Collection
 		{	start = newElement;
 			current = start;
 		} else {
-			resetNext();
+			this.resetNext();
 			while(current.hasNext())
 			{	current = current.getNext();
 			}
@@ -54,7 +57,21 @@ public class Collection
 	public void resetNext()
 	{	current = start;
 	}
-	
+
+	/*
+	calculate the size of the collection from within the Collection class,
+	which is not normally an exam question
+	*/
+	public int size()
+	{	int size = 0;			// or size = 1;
+		this.resetNext();
+		while(current != null)	// or current.hasNext()  with size = 1 above
+		{	size++;
+			this.getNext();
+		}
+		return size;
+	}
+
 	public String toString()
 	{	String output = "";
 		this.resetNext();
--- OOP/IBCollection/src/CollectionDemo.java
+++ OOP/IBCollection/src/CollectionDemo.java
@@ -1,25 +1,35 @@
 public class CollectionDemo
 {
+	// how to calculate the size of a collection outside the Collection class
 	public static int collectionSize(Collection c)
 	{	int counter = 0;
-		Element temp = c.getStart();
-		while(temp != null) {
+		Element temp = c.getStart(); // Element = Node; start @ the beginning of the collection
+		while(temp != null)
+		{	counter++;
 			temp = temp.getNext();
-			counter++;
 		}
 		return counter;
 	}
+
 	public static void main (String[] args)
 	{
 		Collection list = new Collection();
+		System.out.println("Add Ada to the collection");
 		list.addItem("Ada"); System.out.println(list);
+		System.out.println("Ali Ada to the collection");
 		list.addItem("Ali"); System.out.println(list);
+		System.out.println("Add Bob to the collection");
 		list.addItem("Bob"); System.out.println(list);
+		System.out.println("Add Jill to the collection");
 		list.addItem("Jill"); System.out.println(list);
+		System.out.println("Add Kyle to the collection");
 		list.addItem("Kyle");
-		System.out.println("OOOO");
 		System.out.println(list);
+		System.out.print("collectionSize(list) = ");
 		System.out.println(collectionSize(list));
+		System.out.print("list.size() = ");
+		System.out.println(list.size());
+
 	}
 }
 
